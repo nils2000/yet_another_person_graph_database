31e120485815711f31ba861e53ea14fdfce8fbef
Mariam Gichuna Jumaa <mamu@Holgers-Laptop.fritz.box>
2020-05-16 12:07:01 +0200

    gods added

---
+++ graph_gods.py
@@ -0,0 +1,6 @@
+import yapgd
+
+yapgd.add_items("Götter",["Asmodeus","Auril","Azuth","Beshaba","Bhaal","Chauntea","Cyric","Deneir",
+	"Eldath","Gond","Gwaeron Windstrom","Helm","Hoar","Ilmater","Jergal","Kelemvor","Lathander",
+	"Leira","Lliira","Loviatar","Malar","Mask","Mielikki","Milil","Myrkul","Mystra","Oghma",
+	"Die rote Ritterin","Savras","Selûne",""])
